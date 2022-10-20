7e22685005f4d152fd02359e0783e3909daf95e0
Marcelo Téo <marceloteosds.050806@gmail.com>
2022-10-19 20:44:56 -0400

    Projeto  Introdução GIT

---
+++ src/codigo.java
@@ -0,0 +1,6 @@
+public class codigo {
+    public static void main(String[] args) {
+        System.out.println("Nosso primeiro projeto com gerenciamento de versão");
+        System.out.println("Essa é a primeira modificação");
+    }
+}
